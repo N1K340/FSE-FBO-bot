9bacc3aaaea5cc6b454f405dd20198e38177b17d
Nick <77387202+N1K340@users.noreply.github.com>
2024-06-17 20:30:59 +1000

    v2.0 Containerisation
    Rewrote app to combine FBO and Monthly into a single bot.
    -Bot uses python schedule to run at 2200Z and 0900Z
    - Docker file set to have env variables added at runtime

--- README.md
+++ README.md
@@ -1,38 +1,39 @@
 # FSE-FBO-bot
-Simple python script to check supplies and fuel requirements for FBO holdings
+Python script to check supplies and fuel requirements for FBO holdings, and tally monthly hourly rate costs for aircraft
 
 **Script functions:**
 
-fsefbo.py:
+main.py:
 - Check FBO has more than X days of supplies on hand
 - Check FBO which sells JetA has minimum amount on hand (Fuel price > $1)
 - Check FBO which sells Avgas has minimum amount on hand (Fuel price > $1)
+- Check how many hours aircraft flew previous month and calculate the amortised maintenance costs
 - Output results to Discord via Webhook
 
 **Useage**
 
-Using crontab or similar, schedule fsefbo.py to run once or twice per day.
-FSE Datafeeds are limited to 10 polls in 60 seconds, and 40 polls in 6 hours. There is limited value in increasing the fsefbo.py script to run beyond a couple of times a day.
+Intended to be run as a docker container.
+Environmental variables needed to be set for the following API keys
+'FBOHOOK' : The Discord webhook for the FBO check messages to be sent to
+'MXHOOK' : The Discord webhook for the monthly maintenance costs to be sent
+'FSEUSER' : The FSE API key for the user
+'FSEGROUP1' : The FSE API key for the first group of FBO holdings
+'FSEGROUP2' : The FSE API key for the second group of FBO holdings
 
-**Setup**
 
-The following items are required to be set in the 'fsefboconfig' file using a text editor:
-
-*fbowebhook*<br>
+*webhook*<br>
 Create a webhook url on the discord server, selecting the text channel to recieve messages in.
 https://support.discord.com/hc/en-us/articles/228383668-Intro-to-Webhooks
 
 With the generated webhook, paste it into the config file with surrounding "".
 
-*FSE Datafeed keys*<br>
+*FSE API keys*<br>
 This section has three entries required from the FSE website.<br>
 https://server.fseconomy.net/datafeeds.jsp
 
-fseuser is the personal datakey tied to your account.
-fsegroup1 / fsegroup2 are the datakeys for the groups holding the FBO assets.
 This program is not designed for personal FBO holdings, the minimum required is a personal datakey and a group datakey. If second group is not required, leave fsegroup2 blank, i.e. fsegroup2 = ""
 
-*FBO Check Variables*<br>
+*config.py Variables*<br>
 These are the trigger points for the script to decide to notify you of an airport requiring something to be ordered.
 
 supplies_threshold - The minimum number of days where you will start getting warnings that more supplies need to be ordered.
@@ -41,7 +42,13 @@ jet_threshold - The minimum quantity of JetA1 in kilograms that will trigger a w
 
 avgas_threshold - The minimum quantity of Avgas in kilograms that will trigger a warning, providing the FBO sells the fuel for more than $1. If fuel price is set less than $1 the program will assume you wish to not maintain a sellable level on hand and ignore this warning.
 
+aircraft - The dictionary of aircraft registrations that need calculating ie {'A36-002':700, 'A36-003':1000}
+
 **Changelog**
+v2.0
+- Merged FBO and Monthly Maintenance cost scripts together
+- restructured to run as a docker container
+
 v1.1
 - Changed message disemination to use discord webhooks instead of pushing messages to a secondary bot script
 
--- src/app/main.py
+++ src/app/main.py
@@ -13,31 +13,37 @@ v1.1
 - Output results as a discord embeded webhook
 - Refactored code
 
+v2.0
+- Refactored to use schedule library to run as a docker app
+- merged multiple functions to create standalone FSE app
+- functions split out to modules
+
 Configuration required in file 'fsefboconfig.py'
 
 """
 
-import logging
-import os
-import sys
-
 import pandas as pd
+import sys
+import schedule
+import time
+import datetime
+import warnings
+import os
 
 from discord import SyncWebhook, Embed
 from pathlib import Path
 
-from fsefboconfigreal import fbohook_url, supplies_threshold, jet_threshold, avgas_threshold, fseuser, fsegroup1, fsegroup2
+from config import supplies_threshold, jet_threshold, avgas_threshold, aircraft
+fbohook_url = os.environ.get('FBOHOOK', 'Not Set')
+mxhook_url = os.environ.get('MXHOOK', 'Not Set')
+fseuser = os.environ.get('FSEUSER', 'Not Set')
+fsegroup1 = os.environ.get('FSEGROUP1', 'Not Set')
+fsegroup2 = os.environ.get('FSEGROUP2', 'Not Set')
 
 
-# setup the logger
-logger = logging.getLogger(__name__)
-if not os.path.exists(f"{os.path.dirname(__file__)}/logs/"):
-    os.makedirs(f"{os.path.dirname(__file__)}/logs/")
-logging.basicConfig(
-    filename=f"{os.path.dirname(__file__)}/logs/fsefbo.log",
-    format="%(asctime)s:%(message)s",
-    level=logging.INFO
-)
+warnings.filterwarnings("ignore")
+
+'''----------------------- FSE FBO Script ---------------------------'''
 
 # Create dataframe from CSV datafeeds
 def create_data_frame(url1 : str, url2 : str):
@@ -61,8 +67,8 @@ def create_data_frame(url1 : str, url2 : str):
         df = pd.concat([df1, df2], ignore_index=True)
     except:
         print("error getting datafeed")
-        logger.info("\nAn error occoured fetching datafeeds")
-        send_message("\nAn error occoured fetching datafeeds")
+        # logger.info("\nAn error occoured fetching datafeeds")
+        fbo_send_message("\nAn error occoured fetching datafeeds")
         sys.exit(1)
     return df
 
@@ -83,15 +89,16 @@ def create_test_data_frame():
     return df
 
 # Webhook output to Discord
-def send_message(title_str, message_str):
+def fbo_send_message(title_str, message_str):
     """Sends an embed webhook element to the designated discord server.
     
     Args:
         title_str: The title to be used in the embeded discord element.
         message_str: The description to be used in the embeded discord element.
     """
+    print(f'{title_str} : {message_str}')
     webhook = SyncWebhook.from_url(fbohook_url)
-    webhook.send(embed = Embed(title = title_str, description = message_str))
+    webhook.send(embed=Embed(title = title_str, description = message_str))
 
 # Main FBO checks Functions
 
@@ -99,7 +106,7 @@ def fbo_supply_warning(df):
     """Checks for FBOs that might need supplies
     
     Checks FBO listings to isolate airports which have a supplied day value below the threshold preset in config.
-    Generates the Title and Body text for the discord message, and passes them to the send_message function.
+    Generates the Title and Body text for the discord message, and passes them to the fbo_send_message function.
 
     Args:
         df: The main dataframe of FBO listings.
@@ -113,8 +120,8 @@ def fbo_supply_warning(df):
         df.loc[df["SuppliedDays"] < supplies_threshold, "Warning"] = "True"
     except KeyError:
         print("error getting datafeed")
-        logger.info("\nAn error occoured fetching datafeeds")
-        send_message("\nAn error occoured fetching datafeeds")
+        # logger.info("\nAn error occoured fetching datafeeds")
+        fbo_send_message("\nAn error occoured fetching datafeeds")
         sys.exit(1)
 
     airport_warning = df.loc[df["Warning"] == "True"]
@@ -122,20 +129,21 @@ def fbo_supply_warning(df):
     if airport_warning.empty:
         title_str = "FSE FBO Update"
         message_str = "No Airport Supply Warnings"
+        
     else:
         title_str = "FSE FBO Update"
         message_str = "Airports with supply warnings:\n {}".format(
             airport_warning[["Airport", "SuppliedDays"]].to_string(index=False)
         )
-    logger.info("\n{}".format(message_str))
-    send_message(title_str, message_str)
+    # logger.info("\n{}".format(message_str))
+    fbo_send_message(title_str, message_str)
 
 def fbo_jetA_warning(df):
     """Checks for FBOs that might need Jet-A Orders
     
     Checks FBO listings to isolate airports which have Jet-A value below the threshold preset in config.
     Removes airports from the list which do not sell Jet-A.
-    Generates the Title and Body text for the discord message, and passes them to the send_message function.
+    Generates the Title and Body text for the discord message, and passes them to the fbo_send_message function.
 
     Args:
         df: The main dataframe of FBO listings.
@@ -161,15 +169,15 @@ def fbo_jetA_warning(df):
         message_str = "{}".format(
             jeta_resupply[["Airport", "FuelJetA"]].to_string(index=False)
         )
-    logger.info("\n{}".format(message_str))
-    send_message(title_str, message_str)
+    # logger.info("\n{}".format(message_str))
+    fbo_send_message(title_str, message_str)
 
 def fbo_avgas_warning(df):
     """Checks for FBOs that might need Avgas Orders
     
     Checks FBO listings to isolate airports which have Avgas value below the threshold preset in config.
     Removes airports from the list which do not sell Avgas.
-    Generates the Title and Body text for the discord message, and passes them to the send_message function.
+    Generates the Title and Body text for the discord message, and passes them to the fbo_send_message function.
 
     Args:
         df: The main dataframe of FBO listings.
@@ -195,10 +203,10 @@ def fbo_avgas_warning(df):
         message_str = "{}".format(
             avgas_resupply[["Airport", "Fuel100LL"]].to_string(index=False)
         )
-    logger.info("\n{}".format(message_str))
-    send_message(title_str, message_str)
+    # logger.info("\n{}".format(message_str))
+    fbo_send_message(title_str, message_str)
 
-def main(test: bool = False):
+def main_fbo(test: bool = False):
     """Script entry point.
     
     Generates the two URL's to be used to collect the datafeed from FSE.
@@ -208,6 +216,7 @@ def main(test: bool = False):
         test: If True will generate the datafeed from the two test .csv files. False will pull live datafeeds from FSE.
                 Defaults to False
     """
+    print('Running FBO Script')
     dataurl1 = f"https://server.fseconomy.net/data?userkey={fseuser}&format=csv&query=fbos&search=key&readaccesskey={fsegroup1}"
     dataurl2 = f"https://server.fseconomy.net/data?userkey={fseuser}&format=csv&query=fbos&search=key&readaccesskey={fsegroup2}"
     if test:
@@ -219,6 +228,79 @@ def main(test: bool = False):
     fbo_jetA_warning(df)
     fbo_avgas_warning(df)
 
+'''------------------------- MX Monthly Script ----------------------------'''
+# create dataframe from CSV datafeeds
+def calcTargetDate():
+    today = datetime.date.today()
+    first = today.replace(day=1)
+    last_month = first - datetime.timedelta(days=1)
+    target_month = last_month.strftime("%m")
+    target_year = last_month.strftime("%Y")
+    return target_month, target_year
+
+def createurl():
+    target_month, target_year = calcTargetDate()
+    url =  f"https://server.fseconomy.net/data?userkey={fseuser}&format=csv&query=flightlogs&search=monthyear&readaccesskey={fseuser}&month={target_month}&year={target_year}"
+    return url
+
+def convertdec(time_str):
+    hours, minutes = map(int, time_str.split(":"))
+    decimal_time = round((hours + (minutes / 60)), 1)
+    return decimal_time
+
+def createdataframe(url):
+    try:
+        df = pd.read_csv(url)
+        #trim dataframe to only flights
+        df = df[((df.Type == 'flight'))]
+    except:
+        print("Error gettting datafeed")
+        sys.exit(1)
+    return df
+
+def CalculateAircraftCost(df, rego, cost):
+    df1 = df.query("Aircraft == @rego")
+    df1['DecTime'] = df1['FlightTime'].apply(convertdec)
+    TotalHrs = round(df1['DecTime'].sum(), 1)
+    TotalCost = format(round(TotalHrs * cost, 2), '.2f')
+    title_str = f'Monthly totals for {rego}:'
+    message_str = f'{TotalHrs} Hours at ${cost} = ${TotalCost}'
+    mx_send_message(title_str, message_str)
+    
+
+# Webhook output to Discord
+def mx_send_message(title_str, message_str):
+    """Sends an embed webhook element to the designated discord server.
+    
+    Args:
+        title_str: The title to be used in the embeded discord element.
+        message_str: The description to be used in the embeded discord element.
+    """
+    print(f'{title_str} : {message_str}')
+    webhook = SyncWebhook.from_url(mxhook_url)
+    webhook.send(embed=Embed(title = title_str, description = message_str))
+    
+def mxmain():
+    if datetime.date.today().day != 1:
+        return
+    
+    url = createurl()
+    df = createdataframe(url)
+    for airframe, rate in aircraft.items():
+        CalculateAircraftCost(df, airframe, rate)
+
+
+# Task scheduling - Times in UTC for Docker
+def run_check():
+    print(f'{datetime.datetime.now()} fse-bot still running')
+
+schedule.every().day.at("20:00").do(main_fbo)
+schedule.every().day.at("09:00").do(main_fbo)
+schedule.every().day.at("20:00").do(mxmain)
+schedule.every().hour.at(":00").do(run_check)
 
 if __name__ == "__main__":
-    main()
+    print('Starting FSE-Bot Schedule')
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
